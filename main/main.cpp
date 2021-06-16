39715363d77917d7e5d9d5d600f2154c1f35d620
Lucca Silva <luccareinehr@gmail.com>
2021-06-16 18:24:34 -0300

    Add basic SD Card functionality via SPI
    I couldn't make SD MMC, which is a faster protocol, work with my setup.
    It might be because of my old inexpensive SD Card or the
    module used, I don't know.

--- main/main.cpp
+++ main/main.cpp
@@ -7,16 +7,29 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 
+/* clang-format off */
 #include <stdio.h>
 
+#include "src/config.h"
+
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+// IMU
 #include "I2Cbus.hpp"
 #include "MPU.hpp"
 #include "mpu/math.hpp"
 #include "mpu/types.hpp"
-#include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "src/config.h"
+
+// SD Card
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+/* clang-format on */
 
 MPU_t MPU;  // MPU object
 
@@ -38,11 +51,90 @@ void startMPU(MPU_t *MPU) {
     // MPU.setDigitalLowPassFilter(mpud::DLPF_42HZ)
 }
 
+void startSDCard() {
+    esp_err_t ret;
+
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false,   // won't format card if mount fails
+        .max_files = 5,                    // max number of open files
+        .allocation_unit_size = 16 * 1024  // 16 kb allocation unit size
+    };
+
+    sdmmc_card_t *card;
+    const char mount_point[] = MOUNT_POINT;
+    ESP_LOGI("SD", "Initializing SD card");
+    ESP_LOGI("SD", "Using SPI peripheral");
+
+    sdmmc_host_t host = SDSPI_HOST_DEFAULT();
+    host.max_freq_khz =
+        MAX_FREQ_SPI_SDCARD;  // for some reason, the default frequency value
+                              // (20 MHz) issues errors when running
+
+    spi_bus_config_t bus_cfg = {.mosi_io_num = PIN_NUM_MOSI,
+                                .miso_io_num = PIN_NUM_MISO,
+                                .sclk_io_num = PIN_NUM_CLK,
+                                .quadwp_io_num = -1,
+                                .quadhd_io_num = -1,
+                                .max_transfer_sz = 4000,
+                                .flags = 0,
+                                .intr_flags = 0
+    };
+
+    ret = spi_bus_initialize((spi_host_device_t)host.slot, &bus_cfg, 1);
+    if (ret != ESP_OK) {
+        ESP_LOGE("SD", "Failed to initialize bus.");
+        return;
+    }
+
+    // This initializes the slot without card detect (CD -> slot_config.gpio_cd) and write protect (WP -> slot_config.gpio_wp) signals
+    sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
+    slot_config.gpio_cs = PIN_NUM_CS; // CS pin
+    slot_config.host_id = (spi_host_device_t)host.slot;
+
+    ret = esp_vfs_fat_sdspi_mount(mount_point, &host, &slot_config, &mount_config, &card);
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE("SD", "Failed to mount filesystem. "
+                     "If you want the card to be formatted, set the EXAMPLE_FORMAT_IF_MOUNT_FAILED menuconfig option.");
+        } else {
+            ESP_LOGE("SD", "Failed to initialize the card (%s). "
+                     "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+}
+
+void openfileSDCard(FILE *&f, const char *file_name) {
+    char file_dir[30] = MOUNT_POINT"/";
+    strcat(file_dir, file_name);
+    ESP_LOGI("SD", "Opening file %s", file_dir);
+    f = fopen(file_dir, "a");
+    if (f == NULL) {
+        ESP_LOGE("SD", "Failed to open file for writing");
+        return;
+    }
+}
+
+/*void writeinSDCard(FILE *&f, char * string) {
+    fprintf(f, string);
+    ESP_LOGI("SD", "File written");
+}*/
+
+void closefileSDCard(FILE *&f) {
+    fclose(f);
+    ESP_LOGI("SD", "File closed");
+}
+
 void taskAccel(void *params) {
+    FILE *f;
     while (true) {
         /* Acquiring raw acceleratioon data */
         mpud::raw_axes_t accelRaw;    // holds x, y, z axes as int16
         MPU.acceleration(&accelRaw);  // fetch raw data from the registers
+        long time_us = (long)esp_timer_get_time();
         ESP_LOGI("mpu", "accel-raw: %+d %+d %+d\n", accelRaw.x, accelRaw.y,
                  accelRaw.z);
 
@@ -51,6 +143,13 @@ void taskAccel(void *params) {
             accelRaw, ACCELEROMETER_SCALE);  // raw data to gravity
         ESP_LOGI("mpu", "accel: %+.2f %+.2f %+.2f\n", accelG.x, accelG.y,
                  accelG.z);
+
+        /* Writes values to SD Card */
+        openfileSDCard(f, "accel.txt");
+        fprintf(f, "%+.5f,%+.5f,%+.5f,%ld\n", accelG.x, accelG.y,
+                 accelG.z, time_us);
+        closefileSDCard(f);
+
         vTaskDelay(100 / portTICK_PERIOD_MS);
     }
 }
@@ -59,6 +158,12 @@ extern "C" void app_main(void) {
     // Initialize I2C on port 0 using I2Cbus interface
     i2c0.begin(SDA, SCL, CLOCK_SPEED);
     startMPU(&MPU);
+    startSDCard();
 
-    xTaskCreate(&taskAccel, "read accelerometer data", 2048, NULL, 2, NULL);
+    FILE *f;
+    openfileSDCard(f, "accel.txt");
+    fprintf(f, "x(g)     y(g)     z(g)    t(us)\n");
+    closefileSDCard(f);
+
+    xTaskCreate(&taskAccel, "read accelerometer data", 8*1024, NULL, 2, NULL);
 }
--- main/src/config.h
+++ main/src/config.h
@@ -4,18 +4,31 @@
 #ifndef CARDEAL_CONFIG_H_
 #define CARDEAL_CONFIG_H_
 
+#include "MPU.hpp"
+#include "mpu/types.hpp"
+
 /* Settings */
 
 // IMU
-static constexpr uint32_t CLOCK_SPEED = 400000;   // range from 100 KHz ~ 400Hz
-static constexpr uint16_t MPU_SAMPLE_RATE = 100;  // range from 4 Hz ~ 1000 Hz
+static constexpr uint32_t CLOCK_SPEED = 200000;   // range from 100 kHz ~ 400kHz
+static constexpr uint16_t MPU_SAMPLE_RATE = 200;  // range from 4 Hz ~ 1000 Hz
 static constexpr mpud::accel_fs_t ACCELEROMETER_SCALE =
     mpud::ACCEL_FS_4G;  // +- 2g, 4g, 8g or 16g
+
+// SD Card
+#define MOUNT_POINT "/sdcard"
+#define MAX_FREQ_SPI_SDCARD 10000 // in kHz
 
 /* Pinouts */
 
 // I2Cbus lib (IMU dependency)
 static constexpr gpio_num_t SDA = GPIO_NUM_21;
 static constexpr gpio_num_t SCL = GPIO_NUM_22;
+
+// SPI (SD Card)
+static constexpr gpio_num_t PIN_NUM_MISO = GPIO_NUM_2;
+static constexpr gpio_num_t PIN_NUM_MOSI = GPIO_NUM_15;
+static constexpr gpio_num_t PIN_NUM_CLK  = GPIO_NUM_14;
+static constexpr gpio_num_t PIN_NUM_CS   = GPIO_NUM_13;
 
 #endif  // CARDEAL_CONFIG_H_
