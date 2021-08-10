4b3c8c420188f4240a2699fa14f8abec85a094f4
Clang-Format <>
2021-08-10 03:30:10 +0000

    Apply clang-format

--- main/main.cpp
+++ main/main.cpp
@@ -41,7 +41,7 @@
 struct params_taskVoltage_t {
     adc1_channel_t adc1_channel;
     adc_atten_t adc_atten_db;
-    float R1, R2; // if there's no resistors, both 0
+    float R1, R2;  // if there's no resistors, both 0
 };
 
 void taskCurrent(void *params_i2c_address) {
@@ -72,16 +72,18 @@ void taskCurrent(void *params_i2c_address) {
 }
 
 void taskVoltage(void *pvParameters) {
-    params_taskVoltage_t *params = (params_taskVoltage_t *) pvParameters;
+    params_taskVoltage_t *params = (params_taskVoltage_t *)pvParameters;
 
     VoltageSensor sensor;
 
-    sensor.setup(params->adc1_channel, params->adc_atten_db, params->R1, params->R2);
+    sensor.setup(params->adc1_channel, params->adc_atten_db, params->R1,
+                 params->R2);
     sensor.calibLog();
 
     while (true) {
         int value = sensor.read_mV(50);
-        printf("Voltage: %d mV, multiplier: %f\n", value, (params->R1+params->R2)/params->R2);
+        printf("Voltage: %d mV, multiplier: %f\n", value,
+               (params->R1 + params->R2) / params->R2);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
@@ -159,29 +161,52 @@ void taskBMP280(void *pvParameters) {
 }
 
 extern "C" void app_main(void) {
-
     // Task parameters
-    static const struct params_taskVoltage_t BatteryElec = {ADC1_CHANNEL_0, ADC_ATTEN_DB_11, 1, 0.47}; // BAT_ELEC (adc range: 470-7660mV)
-    static const struct params_taskVoltage_t BuckBoostElec = {ADC1_CHANNEL_3, ADC_ATTEN_DB_11, 1, 0.47}; // BUCK (adc range: 470-7660mV)
-    static const struct params_taskVoltage_t BatteryDAQ = {ADC1_CHANNEL_5, ADC_ATTEN_DB_11, 0, 0}; // BAT_ESP (adc range: 150-2450mV)
-    static const struct params_taskVoltage_t StepUpDAQ = {ADC1_CHANNEL_6, ADC_ATTEN_DB_11, 1, 0.47}; // STEPUP (adc range: 470-7660mV)
+    static const struct params_taskVoltage_t BatteryElec = {
+        ADC1_CHANNEL_0, ADC_ATTEN_DB_11, 1,
+        0.47};  // BAT_ELEC (adc range: 470-7660mV)
+    static const struct params_taskVoltage_t BuckBoostElec = {
+        ADC1_CHANNEL_3, ADC_ATTEN_DB_11, 1,
+        0.47};  // BUCK (adc range: 470-7660mV)
+    static const struct params_taskVoltage_t BatteryDAQ = {
+        ADC1_CHANNEL_5, ADC_ATTEN_DB_11, 0,
+        0};  // BAT_ESP (adc range: 150-2450mV)
+    static const struct params_taskVoltage_t StepUpDAQ = {
+        ADC1_CHANNEL_6, ADC_ATTEN_DB_11, 1,
+        0.47};  // STEPUP (adc range: 470-7660mV)
 
     // start i2cdev library, dependency for esp-idf-lib libraries
     ESP_ERROR_CHECK(i2cdev_init());
 
     // Tasks
 
-    xTaskCreate(&taskCurrent, "INA219 battery", configMINIMAL_STACK_SIZE * 8, (void *)INA219_ADDR_GND_GND, 2, NULL); // A1 open A0 open
-    xTaskCreate(&taskCurrent, "INA219 right aileron", configMINIMAL_STACK_SIZE * 8, (void *)INA219_ADDR_GND_VS, 2, NULL); // A1 open A0 bridged
-    xTaskCreate(&taskCurrent, "INA219 right rudder", configMINIMAL_STACK_SIZE * 8, (void *)INA219_ADDR_VS_GND, 2, NULL); // A1 bridged A0 open
-    xTaskCreate(&taskCurrent, "INA219 right elevator", configMINIMAL_STACK_SIZE * 8, (void *)INA219_ADDR_VS_VS, 2, NULL); // A1 bridged A0 bridged
-    
-    xTaskCreate(&taskBMP280, "BMP280 read pressure temp", configMINIMAL_STACK_SIZE * 8, NULL, 3, NULL);
+    xTaskCreate(&taskCurrent, "INA219 battery", configMINIMAL_STACK_SIZE * 8,
+                (void *)INA219_ADDR_GND_GND, 2, NULL);  // A1 open A0 open
+    xTaskCreate(&taskCurrent, "INA219 right aileron",
+                configMINIMAL_STACK_SIZE * 8, (void *)INA219_ADDR_GND_VS, 2,
+                NULL);  // A1 open A0 bridged
+    xTaskCreate(&taskCurrent, "INA219 right rudder",
+                configMINIMAL_STACK_SIZE * 8, (void *)INA219_ADDR_VS_GND, 2,
+                NULL);  // A1 bridged A0 open
+    xTaskCreate(&taskCurrent, "INA219 right elevator",
+                configMINIMAL_STACK_SIZE * 8, (void *)INA219_ADDR_VS_VS, 2,
+                NULL);  // A1 bridged A0 bridged
 
-    xTaskCreate(&taskVoltage, "read battery voltage", configMINIMAL_STACK_SIZE * 8, (void *)&BatteryElec, 2, NULL); // GPIO36 (= VP)
-    xTaskCreate(&taskVoltage, "read regulator voltage", configMINIMAL_STACK_SIZE * 8, (void *)&BuckBoostElec, 2, NULL); // GPIO39 (= VN)
-    xTaskCreate(&taskVoltage, "read DAQ battery voltage", configMINIMAL_STACK_SIZE * 8, (void *)&BatteryDAQ, 2, NULL); // GPIO33
-    xTaskCreate(&taskVoltage, "read DAQ regulator voltage", configMINIMAL_STACK_SIZE * 8, (void *)&StepUpDAQ, 2, NULL); // GPIO34
+    xTaskCreate(&taskBMP280, "BMP280 read pressure temp",
+                configMINIMAL_STACK_SIZE * 8, NULL, 3, NULL);
+
+    xTaskCreate(&taskVoltage, "read battery voltage",
+                configMINIMAL_STACK_SIZE * 8, (void *)&BatteryElec, 2,
+                NULL);  // GPIO36 (= VP)
+    xTaskCreate(&taskVoltage, "read regulator voltage",
+                configMINIMAL_STACK_SIZE * 8, (void *)&BuckBoostElec, 2,
+                NULL);  // GPIO39 (= VN)
+    xTaskCreate(&taskVoltage, "read DAQ battery voltage",
+                configMINIMAL_STACK_SIZE * 8, (void *)&BatteryDAQ, 2,
+                NULL);  // GPIO33
+    xTaskCreate(&taskVoltage, "read DAQ regulator voltage",
+                configMINIMAL_STACK_SIZE * 8, (void *)&StepUpDAQ, 2,
+                NULL);  // GPIO34
 
     xTaskCreate(&taskLoRa_tx, "send LoRa packets", 2048, NULL, 5, NULL);
 }
